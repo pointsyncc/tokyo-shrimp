f2b8f3330208d2bfcb95cf8ce4f492e535102ec5
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2023-02-20 00:15:12 +0100

    fix: Mobile header logo fix

--- src/components/layout/mainLayout/header/Header.tsx
+++ src/components/layout/mainLayout/header/Header.tsx
@@ -1,41 +1,27 @@
-import gsap from 'gsap';
+import logoPrimary from '@/public/imgs/pointsyncc/logo/desktop/transparent-90.png';
+import logoSecondary from '@/public/imgs/pointsyncc/logo/desktop/transparent.png';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useLayoutEffect, useRef } from 'react';
 import { CgMenuGridO } from 'react-icons/cg';
-import logo from '@/public/imgs/pointsyncc/logo/desktop/transparent-90.png';
+
 
 interface HeaderProps {
   openMenu: () => void;
 }
 
 export const Header = ({ openMenu }: HeaderProps) => {
-  // const boxRef = useRef();
-
-  // useLayoutEffect(() => {
-  //   // Refs allow you to access DOM nodes
-  //   console.log(boxRef); // { current: div.box }
-  //   // then we can animate them like so...
-  //   if (boxRef.current) {
-  //     gsap.to(boxRef.current, {
-  //       rotation: "+=360",
-  //     });
-  //   }
-  // });
-
   return (
     <header className='header__area'>
       <div className='header__inner'>
         <div className='header__logo'>
           <Link href='/'>
-            <img
-              className='logo-primary'
-              width={35}
-              src='/imgs/pointsyncc/logo/desktop/transparent-90.png'
-              alt='Site Logo'
+            <Image className='logo-primary' width={35} src={logoPrimary} alt='Pointsyncc Logo' />
+            <Image
+              className='logo-secondary'
+              width={125}
+              src={logoSecondary}
+              alt='Pointsyncc Logo'
             />
-            {/* <Image src={"/imgs/pointsyncc/logo/desktop/transparent-90.png"} fill  className="logo-primary"  alt="Site Logo" /> */}
-            <Image className='logo-secondary' width={125} src={logo} alt='Moibile Logo' />
           </Link>
         </div>
         <div className='header__nav-icon'>
