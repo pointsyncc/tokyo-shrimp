799125ab4370b676bb463218655c780c263dd0e8
Kristijan Vidović <kiki.vidovic.6969@gmail.com>
2023-02-20 00:31:44 +0100

    Feature/repo init (#18) (#19)
    * feat: Add correct info on menu page, replaced all logos with next/image component including next/links, code cleanup, add footer image, typos fix
    
    * fix: Mobile header logo fix
    
    * feat: Add blur to header, mobile header improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        {/* <meta name='viewport' content='width=device-width, initial-scale=1' /> */}
         <link rel='shortcut icon' href='/favicon.svg' />
         <meta property='og:title' content='Pointsyncc' />
         <meta property='og:description' content='Enter the digital world.' />
