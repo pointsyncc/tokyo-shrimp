9878dee80df3804102ecd34587f27a2332ede1b0
muhammadshumas <shumaslaghari@gmail.com>
2023-02-23 01:34:17 +0500

    added yup, select and check, radio inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,13 @@ import { useLayoutEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { useForm } from 'react-hook-form';
-import { Input } from '@/components/forms/controls/Input/Input';
-import { EMAIL_PATTERN } from '@/components/constants/constants';
-import { CheckInput } from '@/components/forms/controls/checkInputGroup/CheckInputGroup';
+// import { Input } from '@/components/forms/controls/Input/Input';
+// import { EMAIL_PATTERN } from '@/components/constants/constants';
+// import { CheckInput } from '@/components/forms/controls/checkInputGroup/CheckInputGroup';
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { SelectInput } from '@/components/forms/controls/selectInput/SelectInput';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
+// import { SelectInput } from '@/components/forms/controls/selectInput/SelectInput';
 
 // Create reusable animations with gsap, create in _app with classes (same as for title)
 
@@ -36,24 +36,24 @@ type IFields = {
   city: string;
 };
 const Home: NextPageWithLayout = () => {
-  const schema = yup.object({
-    email: yup.string().email().required(),
-    password: yup.string().required(),
-    message: yup.string().required(),
-    gender: yup.string().required(),
-    city: yup.string().required(),
-  });
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IFields>({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      email: 'shumaslaghari@gmail.com',
-      city: '',
-    },
-  });
+  // const schema = yup.object({
+  //   email: yup.string().email().required(),
+  //   password: yup.string().required(),
+  //   message: yup.string().required(),
+  //   gender: yup.string().required(),
+  //   city: yup.string().required(),
+  // });
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm<IFields>({
+  //   resolver: yupResolver(schema),
+  //   defaultValues: {
+  //     email: 'shumaslaghari@gmail.com',
+  //     city: '',
+  //   },
+  // });
 
   const onSubmit = (data: any) => {
     console.log(data);
@@ -90,7 +90,7 @@ const Home: NextPageWithLayout = () => {
         <title>Pointsyncc - Home</title>
         <meta name='description' content='Home' />
       </Head>
-      {/* <Hero />
+      <Hero />
       <Roll />
       <About />
       <Service />
@@ -100,9 +100,9 @@ const Home: NextPageWithLayout = () => {
       <Brand />
       <Testimonial />
       <Blog />
-      <CTA /> */}
+      <CTA />
 
-      <form className='contact__form-2' onSubmit={handleSubmit(onSubmit)}>
+      {/* <form className='contact__form-2' onSubmit={handleSubmit(onSubmit)}>
         <div className='row g-3'>
           <div className='col-xxl-6 col-xl-6 col-12'>
             <Input
@@ -155,34 +155,6 @@ const Home: NextPageWithLayout = () => {
               ]}
               register={register}
             />
-
-            {/* <Input
-              errors={errors}
-              className='form-check-input'
-              type='radio'
-              name='gender'
-              label='Male'
-              value='male'
-              register={register}
-              labelProps={{ required: true }}
-              rules={{
-                required: 'Gender is required',
-              }}
-            /> */}
-
-            {/* <Input
-              errors={errors}
-              className='form-check-input'
-              type='radio'
-              name="gender"
-              register={register}
-              label='Female'
-              value='female'
-              labelProps={{ required: true }}
-              rules={{
-                required: 'Gender is required',
-              }}
-            /> */}
           </div>
         </div>
         <div className='btn_wrapper'>
@@ -191,7 +163,7 @@ const Home: NextPageWithLayout = () => {
             Messages <i className='fa-solid fa-arrow-right'></i>
           </button>
         </div>
-      </form>
+      </form> */}
     </div>
   );
 };
