7562a06c528a421316d33f43ab2516db76ce2b04
muhammadshumas <shumaslaghari@gmail.com>
2023-03-22 00:25:41 +0500

    DEV-20:removed cookie banner overlay and moved it a little up from the bottom edge

--- src/components/common/cookieConsent/CookieConsent.tsx
+++ src/components/common/cookieConsent/CookieConsent.tsx
@@ -1,21 +1,24 @@
+import { classNames } from '@/utils/classNames';
 import React from 'react';
 import CookieConsent from 'react-cookie-consent';
+import classes from './cookieConsent.module.scss'
 export const CookieBanner = () => {
   return (
     <CookieConsent
       enableDeclineButton
-      overlay={true}
       acceptOnScroll={true}
       acceptOnScrollPercentage={10}
       debug={false}
       style={{
-        background:'red',
+        background: 'red',
         maxWidth: '430px',
-
-        inset:'auto 10px 10px auto',
+        inset: 'auto 10px 10px auto',
       }}
       contentStyle={{ margin: 0, flexBasis: 'auto' }}
-      containerClasses='p-3 flex-column border border-white rounded text-light bg-black'
+      containerClasses={classNames(
+        classes['cookie-consent'],
+        'p-3 flex-column border border-white rounded text-light bg-black',
+      )}
       buttonClasses='btn btn-outline-light bg-transparent m-0 border border-white text-white rounded d-block ms-auto'
       declineButtonText='Opt-out'
       buttonWrapperClasses='d-flex w-100'
@@ -24,7 +27,9 @@ export const CookieBanner = () => {
         width: '120px',
       }}
     >
-      <p className='text-white mb-4'>This site uses cookies to measure and improve your experience.</p>
+      <p className='text-white mb-4'>
+        This site uses cookies to measure and improve your experience.
+      </p>
     </CookieConsent>
   );
 };
