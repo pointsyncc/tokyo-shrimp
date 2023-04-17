2bddb5107869b73a89ff3a628153c555407fe768
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2023-04-17 23:21:55 +0200

    feat: Show navbar on scroll down and hide on scroll up

--- src/components/layout/mainLayout/header/Header.tsx
+++ src/components/layout/mainLayout/header/Header.tsx
@@ -5,62 +5,91 @@ import { CgMenuGridO } from 'react-icons/cg';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaPaperPlane, FaTwitter } from 'react-icons/fa';
 import Link from 'next/link';
-
+import { useEffect, useState } from 'react';
 
 interface HeaderProps {
   openMenu: () => void;
 }
 
 export const Header = ({ openMenu }: HeaderProps) => {
+  const { t } = useTranslation('common');
 
-  const {t} = useTranslation('common');
+  const { matches } = useMatchMedia('(max-width: 450px)');
 
-  const {matches} = useMatchMedia("(max-width: 450px)")
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    let previousScrollPosition = 0;
+    let currentScrollPosition = 0;
+
+    window.addEventListener('scroll', function (e) {
+      // Get the new Value
+      currentScrollPosition = window.pageYOffset;
+
+      //Subtract the two and conclude
+      if (previousScrollPosition - currentScrollPosition < 0) {
+        setShow(false);
+      } else if (previousScrollPosition - currentScrollPosition > 0) {
+        setShow(true);
+      }
+
+      // Update the previous value
+      previousScrollPosition = currentScrollPosition;
+    });
+  }, []);
 
   return (
-    <header className='header__area'>
-      <div className='header__inner'>
-        <div className='header__logo'>
-          {/* <Link href='/'> */}
-            <Logo/>
+    <>
+    {show && (
+      <header className='header__area'>
+        <div className='header__inner'>
+          <div className='header__logo'>
+            {/* <Link href='/'> */}
+            <Logo />
             {/* <Image className='logo-primary' width={30} src={logoPrimary} alt='Pointsyncc Logo' /> */}
-            <Logo type="secondary"/>
+            <Logo type='secondary' />
             {/* <Image
               className='logo-secondary'
               width={125}
               src={logoSecondary}
               alt='Pointsyncc Logo'
             /> */}
-           {/* </Link> */}
-        </div>
-        <div className='header__nav-icon'>
-          <button
-            id='open_offcanvas'
-            onClick={() => {
-              openMenu();
-            }}
-          >
-            {matches ? <RxHamburgerMenu fontSize={'1.75rem'} className='text-white icon__social' /> : <CgMenuGridO fontSize={'2rem'} className='text-white icon__social' />}
-          </button>
-        </div> 
-        <div className='header__support d-none d-xl-flex flex-column justify-content-center gap-3 '>
-          {/* <p>
+            {/* </Link> */}
+          </div>
+          <div className='header__nav-icon'>
+            <button
+              id='open_offcanvas'
+              onClick={() => {
+                openMenu();
+              }}
+            >
+              {matches ? (
+                <RxHamburgerMenu fontSize={'1.75rem'} className='text-white icon__social' />
+              ) : (
+                <CgMenuGridO fontSize={'2rem'} className='text-white icon__social' />
+              )}
+            </button>
+          </div>
+          <div className='header__support d-none d-xl-flex flex-column justify-content-center gap-3 '>
+            {/* <p>
             {t('dictionary.contact-us')} <a href='tel:+9587325902'>+385 99 2144 802</a>
           </p> */}
-          {/* <Link href='https://www.facebook.com/pointsyncc' target='_blank'>
+            {/* <Link href='https://www.facebook.com/pointsyncc' target='_blank'>
             <FaFacebookF fontSize={'1.5rem'} className='text-white' />
           </Link> */}
-          <Link href='https://www.instagram.com/pointsyncc/'  target='_blank'>
-            <FaInstagram fontSize={'1.35rem'} className='icon__social icon__header' />
-          </Link>
-          <Link href='https://www.linkedin.com/company/pointsyncc/'  target='_blank'>
-            <FaLinkedinIn fontSize={'1.35rem'} className='icon__social icon__header' />
-          </Link>
-          {/* <Link href='https://twitter.com/pointsyncc' target='_blank'>
+            <Link href='https://www.instagram.com/pointsyncc/' target='_blank'>
+              <FaInstagram fontSize={'1.35rem'} className='icon__social icon__header' />
+            </Link>
+            <Link href='https://www.linkedin.com/company/pointsyncc/' target='_blank'>
+              <FaLinkedinIn fontSize={'1.35rem'} className='icon__social icon__header' />
+            </Link>
+            {/* <Link href='https://twitter.com/pointsyncc' target='_blank'>
             <FaTwitter fontSize={'1.5rem'} className='text-white' />
           </Link> */}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    )}
+    </>
   );
 };
--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -7,7 +7,7 @@ import gsap from 'gsap';
 import { Link as RouteTranslateLink } from 'next-translate-routes';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'next-i18next';
 import { FaTimes } from 'react-icons/fa';
 import LocaleSwitcher from '../common/localeSwitcher/LocaleSwitcher';
@@ -99,136 +99,137 @@ export default function Menu({ open, setOpen, showLangSwitcher }: IProps) {
     },
   ];
   return (
-    <div
-      className={`offcanvas__area ${classes.offcanvas}`}
-      style={{
-        opacity: open ? 1 : 0,
-        visibility: open ? 'visible' : 'hidden',
-      }}
-    >
-      <div className='offcanvas__body'>
-        <div className='offcanvas__left'>
-          <div className='offcanvas__logo'>
-            {/* <Link href='/'> */}
-            <Logo className='logo' type='secondary' width={180} />
-            {/* <img
+    <>
+      <div
+        className={`offcanvas__area ${classes.offcanvas}`}
+        style={{
+          opacity: open ? 1 : 0,
+          visibility: open ? 'visible' : 'hidden',
+        }}
+      >
+        <div className='offcanvas__body'>
+          <div className='offcanvas__left'>
+            <div className='offcanvas__logo'>
+              {/* <Link href='/'> */}
+              <Logo className='logo' type='secondary' width={180} />
+              {/* <img
                 width={180}
                 src='imgs/pointsyncc/logo/desktop/transparent.png'
                 alt='PointSync logo'
               /> */}
-            {/* </Link> */}
-          </div>
-          <div className='offcanvas__social'>
-            <h3 className='social-title'>{t('menu.social.follow-us')}</h3>
-            <ul>
-              <li>
-                <Link href='https://www.instagram.com/pointsyncc/' target={'_blank'}>
-                  Instagram
-                </Link>
-              </li>
-              <li>
-                <Link href='https://www.facebook.com/pointsyncc/' target={'_blank'}>
-                  Facebook
-                </Link>
-              </li>
-              <li>
-                <Link href='https://www.linkedin.com/pointsyncc/' target={'_blank'}>
-                  LinkedIn
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className='offcanvas__links'>
-            {/* <h3 className='social-title'>Trending</h3> */}
-            <ul>
-              {/* <li>
+              {/* </Link> */}
+            </div>
+            <div className='offcanvas__social'>
+              <h3 className='social-title'>{t('menu.social.follow-us')}</h3>
+              <ul>
+                <li>
+                  <Link href='https://www.instagram.com/pointsyncc/' target={'_blank'}>
+                    Instagram
+                  </Link>
+                </li>
+                <li>
+                  <Link href='https://www.facebook.com/pointsyncc/' target={'_blank'}>
+                    Facebook
+                  </Link>
+                </li>
+                <li>
+                  <Link href='https://www.linkedin.com/pointsyncc/' target={'_blank'}>
+                    LinkedIn
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className='offcanvas__links'>
+              {/* <h3 className='social-title'>Trending</h3> */}
+              <ul>
+                {/* <li>
                 <RouteTranslateLink href='/about'>{t('dictionary.about')}</RouteTranslateLink>
               </li> */}
-              <li>
-                <RouteTranslateLink href='/contact'>{t('dictionary.contact')}</RouteTranslateLink>
-              </li>
-              <li>
-                <RouteTranslateLink href='/career'>{t('dictionary.career')}</RouteTranslateLink>
-              </li>
-              <li>
-                <RouteTranslateLink href='/blog'>{t('dictionary.blog')}</RouteTranslateLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className='offcanvas__mid d-flex justify-content-center  flex-column'>
-          <div className='offcanvas__menu-wrapper'>
-            <nav className={`offcanvas__menu ${classes['offcanvas__menu']}`}>
-              <ul className='menu-anim d-flex align-items-stretch flex-column justify-content-center text-center gap-2'>
-                {links.map((link) => (
-                  <li key={link.href}>
-                    <RouteTranslateLink href={link.href}>{link.text}</RouteTranslateLink>
-                  </li>
-                ))}
+                <li>
+                  <RouteTranslateLink href='/contact'>{t('dictionary.contact')}</RouteTranslateLink>
+                </li>
+                <li>
+                  <RouteTranslateLink href='/career'>{t('dictionary.career')}</RouteTranslateLink>
+                </li>
+                <li>
+                  <RouteTranslateLink href='/blog'>{t('dictionary.blog')}</RouteTranslateLink>
+                </li>
               </ul>
-            </nav>
+            </div>
           </div>
-          {showLangSwitcher ? (
-            <LocaleSwitcher
-              className={classNames(
-                'align-self-center mt-5',
-                'd-flex d-xl-none',
-              )}
-              contentZIndex={10000}
-            />
-          ) : null}
-        </div>
-        <div className='offcanvas__right'>
-          <div className='offcanvas__search'>
-            {/* WE NEED TO MAKE THIS WORK LATER ON WITH REAL FUNCTIONALITY */}
-            {/* <form action='#'>
+          <div className='offcanvas__mid d-flex justify-content-center  flex-column'>
+            <div className='offcanvas__menu-wrapper'>
+              <nav className={`offcanvas__menu ${classes['offcanvas__menu']}`}>
+                <ul className='menu-anim d-flex align-items-stretch flex-column justify-content-center text-center gap-2'>
+                  {links.map((link) => (
+                    <li key={link.href}>
+                      <RouteTranslateLink href={link.href}>{link.text}</RouteTranslateLink>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            {showLangSwitcher ? (
+              <LocaleSwitcher
+                className={classNames('align-self-center mt-5', 'd-flex d-xl-none')}
+                contentZIndex={10000}
+              />
+            ) : null}
+          </div>
+          <div className='offcanvas__right'>
+            <div className='offcanvas__search'>
+              {/* WE NEED TO MAKE THIS WORK LATER ON WITH REAL FUNCTIONALITY */}
+              {/* <form action='#'>
               <input type='text' name='search' placeholder='Search keyword' />
               <button>
                 <i className='fa-solid fa-magnifying-glass'></i>
               </button>
             </form> */}
+            </div>
+            <div className='offcanvas__contact'>
+              <h3>{t('menu.get-in-touch')}</h3>
+              <ul>
+                <li>
+                  <a href={`tel:${COMPANY_CONTACT_PHONE_NUMBER}`}>{COMPANY_CONTACT_PHONE_NUMBER}</a>
+                </li>
+                <li>
+                  <a href={`mailto:${COMPANY_CONTACT_EMAIL}`}>{COMPANY_CONTACT_EMAIL}</a>
+                </li>
+                <li>
+                  <a href={`https://goo.gl/maps/fRA3HnsYcRCNwjd28`} target='_blank'>
+                    {COMPANY_ADDRESS}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <Image
+              style={{ height: 'auto' }}
+              raw={true}
+              src='/imgs/shape/11.png'
+              alt='shape'
+              className='shape-1'
+              width={189}
+              height={94}
+            />
+            {/* <img src='/imgs/shape/11.png' alt='shape' className='shape-1' /> */}
+            <Image
+              raw={true}
+              width={81}
+              height={80}
+              style={{ height: 'auto' }}
+              src='/imgs/shape/12.png'
+              alt='shape'
+              className='shape-2'
+            />
+            {/* <img src='/imgs/shape/12.png' alt='shape' className='shape-2' /> */}
           </div>
-          <div className='offcanvas__contact'>
-            <h3>{t('menu.get-in-touch')}</h3>
-            <ul>
-              <li>
-                <a href={`tel:${COMPANY_CONTACT_PHONE_NUMBER}`}>{COMPANY_CONTACT_PHONE_NUMBER}</a>
-              </li>
-              <li>
-                <a href={`mailto:${COMPANY_CONTACT_EMAIL}`}>{COMPANY_CONTACT_EMAIL}</a>
-              </li>
-              <li>
-                <a href={`https://goo.gl/maps/fRA3HnsYcRCNwjd28`} target='_blank'>{COMPANY_ADDRESS}</a>
-              </li>
-            </ul>
+          <div className='offcanvas__close'>
+            <button type='button' id='close_offcanvas' onClick={setOpen.bind(null, false)}>
+              <FaTimes />
+            </button>
           </div>
-          <Image
-            style={{ height: 'auto' }}
-            raw={true}
-            src='/imgs/shape/11.png'
-            alt='shape'
-            className='shape-1'
-            width={189}
-            height={94}
-          />
-          {/* <img src='/imgs/shape/11.png' alt='shape' className='shape-1' /> */}
-          <Image
-            raw={true}
-            width={81}
-            height={80}
-            style={{ height: 'auto' }}
-            src='/imgs/shape/12.png'
-            alt='shape'
-            className='shape-2'
-          />
-          {/* <img src='/imgs/shape/12.png' alt='shape' className='shape-2' /> */}
-        </div>
-        <div className='offcanvas__close'>
-          <button type='button' id='close_offcanvas' onClick={setOpen.bind(null, false)}>
-            <FaTimes />
-          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
