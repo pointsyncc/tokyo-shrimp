4bc24d71c44dd49a49005b167dbbb0da7a000f6f
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2023-04-18 18:40:31 +0200

    Add content images, code cleanup

--- src/components/home/Testimonial.tsx
+++ src/components/home/Testimonial.tsx
@@ -73,7 +73,7 @@ export default function Testimonial() {
                   style={{ position: 'relative', height: 'auto' }}
                   width={220}
                   height={300}
-                  src='/imgs/testimonial/1/3.png'
+                  src='https://tokyo.fra1.cdn.digitaloceanspaces.com/projects/tokyo-shrimp/testimonials/pointsyncc-testimonial-ivana-kurtov-220x300-01.jpg'
                   alt='Testimonial Image'
                 />
 
@@ -91,7 +91,7 @@ export default function Testimonial() {
                   style={{ position: 'relative', height: 'auto' }}
                   width={260}
                   height={360}
-                  src='/imgs/testimonial/1/2.png'
+                  src='https://tokyo.fra1.cdn.digitaloceanspaces.com/projects/tokyo-shrimp/testimonials/pointsyncc-testimonial-marijan-markovic-260x360-01.jpg'
                   alt='Testimonial Image'
                 />
 
