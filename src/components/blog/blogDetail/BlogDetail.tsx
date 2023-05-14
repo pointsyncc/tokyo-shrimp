9fc2b6a16713a0f01e7aad5faec1150ef46a08b9
Kristijan Vidović <kristijan.vidovic@pointsyncc.com>
2023-05-14 17:10:53 +0200

    feat: Styling improvements

--- src/components/blog/blogDetail/BlogDetail.tsx
+++ src/components/blog/blogDetail/BlogDetail.tsx
@@ -170,7 +170,7 @@ export const BlogDetail = ({
           </div>
           <div className='col-xxl-8 col-xl-10 offset-xxl-2 offset-xl-1'>
             <div className='blog__detail-content'>{render(content)}</div>
-            <div className='d-flex  flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-3 gap-md-0'>
+            <div className='d-flex  flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-4 gap-md-0'>
               <div className='blog__detail-tags'>
                 <p className='sub-title-anim'>
                   {t('blog.tags', { ns: 'blog' })}:{' '}
