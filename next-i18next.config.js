51de11f628c902dc285bebcfa496b7bfad9dd2b7
Kristijan Vidović <kristijan.vidovic@pointsyncc.com>
2023-05-31 18:19:29 +0200

    feat: Switch to local translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,5 @@
-const isBrowser = typeof window !== 'undefined';
-
+/* const isBrowser = typeof window !== 'undefined';
+ */
 
 // next-i18next.config.js
 module.exports = {
@@ -7,7 +7,8 @@ module.exports = {
     defaultLocale: 'hr',
     locales: ['hr','en', 'de'],
   },
-  backend: {
+  ns: ['common', 'footer', 'homepage', 'cookie-consent', 'contact', 'team', 'about', 'seo', 'blog', '404', '505', 'terms-and-conditions', 'privacy-policy'], // the namespaces needs to be listed here, to make sure they got preloaded
+/*   backend: {
     projectId: process.env.NEXT_PUBLIC_LOCIZE_PROJECT_ID,
     version: process.env.NEXT_PUBLIC_LOCIZE_VERSION,
     apiKey: process.env.NEXT_PUBLIC_LOCIZE_API_KEY, // to not add the api-key in production, used for saveMissing feature
@@ -17,8 +18,7 @@ module.exports = {
   use: [
     require('i18next-locize-backend/cjs')
   ],
-  ns: ['common', 'footer', 'homepage', 'cookie-consent', 'contact', 'team', 'about', 'seo', 'blog', '404', '505', 'terms-and-conditions', 'privacy-policy'], // the namespaces needs to be listed here, to make sure they got preloaded
   serializeConfig: false, // because of the custom use i18next plugin
   debug: process.env.NODE_ENV === 'development',
-  saveMissing: process.env.NODE_ENV === 'development' && isBrowser, // to not saveMissing to true for production
+  saveMissing: process.env.NODE_ENV === 'development' && isBrowser, // to not saveMissing to true for production */
 }
