4daf35ec9f2a60a575819b7884bd769dc97ba872
Kristijan Vidović <kristijan.vidovic@pointsyncc.com>
2023-05-31 18:38:03 +0200

    feat: Fix local translations not working

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,8 +7,8 @@ module.exports = {
     defaultLocale: 'hr',
     locales: ['hr','en', 'de'],
   },
-  ns: ['common', 'footer', 'homepage', 'cookie-consent', 'contact', 'team', 'about', 'seo', 'blog', '404', '505', 'terms-and-conditions', 'privacy-policy'], // the namespaces needs to be listed here, to make sure they got preloaded
-/*   backend: {
+/*   ns: ['common', 'footer', 'homepage', 'cookie-consent', 'contact', 'team', 'about', 'seo', 'blog', '404', '505', 'terms-and-conditions', 'privacy-policy'], // the namespaces needs to be listed here, to make sure they got preloaded
+ *//*   backend: {
     projectId: process.env.NEXT_PUBLIC_LOCIZE_PROJECT_ID,
     version: process.env.NEXT_PUBLIC_LOCIZE_VERSION,
     apiKey: process.env.NEXT_PUBLIC_LOCIZE_API_KEY, // to not add the api-key in production, used for saveMissing feature
